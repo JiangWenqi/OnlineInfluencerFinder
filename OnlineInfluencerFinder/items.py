95fa835a33ed27e269d977d8e87e5b10b1782f49
Vinci <Jiangwenqi1995@gmail.com>
2021-04-06 11:37:59 +0800

    Fix capitalization problem

--- OnlineInfluencerFinder/items.py
+++ OnlineInfluencerFinder/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class OnlineinfluencerfinderItem(scrapy.Item):
+class OnlineInfluencerFinderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
--- OnlineInfluencerFinder/middlewares.py
+++ OnlineInfluencerFinder/middlewares.py
@@ -9,7 +9,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-class OnlineinfluencerfinderSpiderMiddleware:
+class OnlineInfluencerFinderSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -56,7 +56,7 @@ class OnlineinfluencerfinderSpiderMiddleware:
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class OnlineinfluencerfinderDownloaderMiddleware:
+class OnlineInfluencerFinderDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
